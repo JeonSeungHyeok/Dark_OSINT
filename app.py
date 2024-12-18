14a9702aa3952fa72d4070f03f01eda2a0b1dcd6
SEOGEONWOO <gotfh3@naver.com>
2024-12-18 10:28:25 +0900

    feat. medusa 항목 추가
    추가 완료

--- app.py
+++ app.py
@@ -1,43 +1,60 @@
 from default.basic_tor import *
-from medusa.medusa import *
-from collections import OrderedDict
-from captcha import *
+from blackbasta.blackbasta import *
+from play.play import *
+from rhysida.rhysida import *
+from elastic import ELK
 import json
 import os
 
 def reorder_dict(data):
-    desired_order = ["title", "description", "timer", "price", "views", "update_date", "site", "address", "country", "region", "link"]
+    desired_order = ["title", "Description", "site", "address", "country", "region", "all data", "tel", "link", "images", "times", "timer", "price"]
     reordered = {}
     for key, value in data.items():
         if isinstance(value, dict):
             reordered[key] = {k: value.get(k,"N/A") for k in desired_order}
         else:
             reordered[key] = value
+
+    if len(reordered)==0:
+        tmp = {}
+        for desired_key in desired_order:
+            tmp[desired_key] = "N/A"
+        reordered["N/A"]=tmp
+                   
     return reordered
 
 def make_output_file(name,result):
     current_path = os.getcwd()
     try:
          os.mkdir("OUT")
-    except FileExistsError:
+    except FileExistsError as e:
          pass
-    file_path = f"{current_path}/OUT/{name}_result.json"  # file_path 정의
-    with open(file_path, "w") as json_file:
+    with open(f"{current_path}/OUT/{name}_result.json", "w") as json_file:
             json.dump(reorder_dict(result), json_file, indent=4)
-    return file_path
 
 def process():
     urls = {
-        "medusa": "http://xfv4jzckytb4g3ckwemcny3ihv4i5p4lqzdpi624cxisu35my5fwi5qd.onion/",
-        #"rhysida":"http://rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.onion",
+        "blackbasta":"http://stniiomyjliimcgkvdszvgen3eaaoz55hreqqx6o77yvmpwt7gklffqd.onion/",
+        "play": "http://mbrlkbtq5jonaqkurjwmxftytyn2ethqvbxfu4rgjbkkknndqwae6byd.onion/",
+        "rhysida":"http://rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.onion",
     }
     classes = {
-        "medusa":osint_medusa,
-        #"rhysida":osint_rhysida,
+        "blackbasta":osint_blackbasta,
+        "play":osint_play,
+        "rhysida":osint_rhysida,
     }
-    result = []
+    js = ['blackbasta','play','rhysida']
+    tmp = osint_tor_render_js()
+    tmp.init_browser()
     for key,value in classes.items():
         osint_class = value(urls[key])
-        file_path = make_output_file(key, osint_class.process())
-        result.append(file_path)
-    return result
+        if key in js:
+            osint_class.browser = tmp.browser
+            osint_class.page = tmp.page
+            result, tmp.browser, tmp.page = osint_class.process()
+        else:
+             result = osint_class.process()
+        make_output_file(key,result)
+    tmp.close_browser()
+    
+    ELK().process()
