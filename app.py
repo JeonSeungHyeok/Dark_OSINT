3e02c0b81c37ced8fee837d8b2a1b22221ed5560
SEOGEONWOO <gotfh3@naver.com>
2024-12-18 20:21:03 +0900

    feat. app.py 수정 및 추가
    app.py 수정 및 추가

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from default.basic_tor import *
 from blackbasta.blackbasta import *
 from play.play import *
 from rhysida.rhysida import *
+#from bianlian.bianlian import *
 from medusa.medusa import *
 from elastic import ELK
 import json
 import os
 
 def reorder_dict(data):
-    desired_order = ["title", "Description", "site", "address", "country", "region", "all data", "tel", "link", "images", "times", "timer", "price"]
+    desired_order = ["title", "description", "update_date", "timer", "price", "country", "region", "views"]
     reordered = {}
     for key, value in data.items():
         if isinstance(value, dict):
@@ -38,15 +39,17 @@ def process():
         #"blackbasta":"http://stniiomyjliimcgkvdszvgen3eaaoz55hreqqx6o77yvmpwt7gklffqd.onion/",
         #"play": "http://mbrlkbtq5jonaqkurjwmxftytyn2ethqvbxfu4rgjbkkknndqwae6byd.onion/",
         #"rhysida":"http://rhysidafohrhyy2aszi7bm32tnjat5xri65fopcxkdfxhi4tidsg7cad.onion",
+        #"bianlian": "http://bianlianlbc5an4kgnay3opdemgcryg2kpfcbgczopmm3dnbz3uaunad.onion/",
         "medusa": "http://xfv4jzckytb4g3ckwemcny3ihv4i5p4lqzdpi624cxisu35my5fwi5qd.onion/",
     }
     classes = {
         #"blackbasta":osint_blackbasta,
         #"play":osint_play,
         #"rhysida":osint_rhysida,
+        #"bianlian": osint_bianlian,
         "medusa":osint_medusa,
     }
-    js = ['blackbasta','play','rhysida','medusa']
+    js = ['blackbasta','play','rhysida', 'bianlian', 'medusa']
     tmp = osint_tor_render_js()
     tmp.init_browser()
     for key,value in classes.items():
@@ -60,4 +63,4 @@ def process():
         make_output_file(key,result)
     tmp.close_browser()
     
-    #ELK().process()
+    ELK().process()
