ddeca05d3afcb758b7eaca921ad4edf4129567ab
JeonSeungHyeok <victory_hyeok@naver.com>
2024-12-19 11:09:05 +0900

    modify

--- app/rhysida/rhysida.py
+++ app/rhysida/rhysida.py
@@ -6,14 +6,12 @@ import requests
 class osint_rhysida(osint_tor_render_js):
     def __init__(self, url):
         super().__init__(url)
+        self.progress=True
 
     def using_bs4(self):
         html = self.response.text
         bsobj = BeautifulSoup(html,'html.parser')
         object_table = bsobj.find("div",class_="carousel-inner")
-        if not object_table:
-            return
-        
         object_table = object_table.find_all("div",class_="carousel-item")
         for item in object_table:
             mass = item.find('div',class_="col-8")
