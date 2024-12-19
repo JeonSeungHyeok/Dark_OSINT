419a115623271aa87d68cf50155f7931fc995fbe
SEOGEONWOO <gotfh3@naver.com>
2024-12-19 12:01:42 +0900

    feat. telegram.py 수정(N/A, 공백 필터)
    telegram.py 수정

--- telegram/telegram.py
+++ telegram/telegram.py
@@ -1,15 +1,19 @@
 import requests
-
+import json
 
 BOT_TOKEN = '7612275874:AAHkZ3F1-xNyDdV8jJYhS9MXbN3xqSnI3l8'
 CHAT_ID = '-1002438870737'
 url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
 
-def send_message(name, message):
+def send_telegram(name, value, message):
     payload = {
-    'chat_id': CHAT_ID,
-    'text': '#'+name+'\n'+message
+        'chat_id': CHAT_ID,
+        'text': f'#{name}\n{value}: {message}'
     }
     requests.post(url, data=payload)
 
-
+def send_message(name, message):
+    for value in message:
+        filtered_message = {k: v for k, v in message[value].items() if v != "N/A" and v != []}
+        if filtered_message:  
+            send_telegram(name, value, json.dumps(filtered_message, indent=4))
